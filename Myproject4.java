0dd5fa3a7ba0c3e837e35835d27fbe36f8833a19
Napier <71715345+22p21s0045@users.noreply.github.com>
2023-03-21 15:19:19 +0700

    change

--- Myproject4.java
+++ Myproject4.java
@@ -14,7 +14,7 @@ public class Myproject4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello worlds !");
+        System.out.println("Hello worlds title !");
         // TODO code application logic here
     }
     
